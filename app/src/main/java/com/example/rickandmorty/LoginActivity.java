10595d3ba8767a26e000ac810836c66420c00f5b
nixonBravo <nixonelian2002@gmail.com>
2023-11-23 20:20:40 -0500

    para ver los cambios

--- app/src/main/java/com/example/rickandmorty/LoginActivity.java
+++ app/src/main/java/com/example/rickandmorty/LoginActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.rickandmorty.api.RetrofitClientLocal;
 import com.example.rickandmorty.resource.local.AutenticacionService;
